bb92176147c8e3bfeb7361b855f7b48d32afac80
强强爱娜娜 <qiangqiangainana@192.168.1.102>
2019-12-08 16:10:01 +0800

    新增文件

--- src/main/scala/table/TableTest.scala
+++ src/main/scala/table/TableTest.scala
@@ -1,7 +1,9 @@
 package table
 
-import org.apache.flink.api.common.functions.{FlatMapFunction, MapFunction}
+import org.apache.flink.api.common.functions.{FlatMapFunction, MapFunction, RichFlatMapFunction}
+import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
 import org.apache.flink.api.scala._
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
@@ -34,7 +36,7 @@ object TableTest {
     })
     tableEnv.registerDataStream("table1", inputDataResult, 'field1, 'field2)
     val table2 = tableEnv.fromDataStream(inputDataResult1, 'field3, 'field4)
-    val projTable = tableEnv.scan("table1").join(table2).where('field1 === 'field3).select('field1,'field2,'field4)
+    val projTable = tableEnv.scan("table1").leftOuterJoin(table2).where('field1 === 'field3).select('field1,'field2,'field4)
 //    val sinkTable = projTable.addSink(new SinkFunction[Row] {
 //      override def invoke(value: Row, context: SinkFunction.Context[_]): Unit = {
 //        println(row)
@@ -49,11 +51,36 @@ object TableTest {
       }
     })
 
-    val groupResult = result.keyBy(0).timeWindow(Time.seconds(5)).sum(0)
+    val groupResult = result.keyBy(0).flatMap(new RichFlatMapFunction[(Long, String, String),(Long, String, String)] {
+
+      private var valueState: ValueState[(Long, String, String)] = _
+        override def open(parameters: Configuration): Unit = {
+          val stateDescriptor = new ValueStateDescriptor[(Long,String,String)]("tableTest",classOf[(Long, String, String)])
+          valueState = getRuntimeContext.getState(stateDescriptor)
+        }
+
+        override def flatMap(in: (Long, String, String), collector: Collector[(Long, String, String)]): Unit = {
+          val value = valueState.value()
+          println("value -> ", value)
+          if(value != null && value._1 == in._1) {
+            collector.collect((value._1, value._2, in._3))
+          } else {
+            collector.collect(in)
+          }
+          valueState.update(in)
+      }
+    })
+//      .keyBy(0).timeWindow(Time.seconds(5), Time.seconds(3)).sum(0)
 
     groupResult.print()
 
     env.execute("table test")
 
   }
+
+  case class Person(val id: Long, val name1: String, val name2: String) {
+
+  }
 }
+
+
